de4cb17c562be2df5e6fc2a0b26321fe5334acc7
haeunko <hekoh99@naver.com>
2022-06-28 15:04:58 +0900

    check data

--- kaggle/catndog/CatVsDog.py
+++ kaggle/catndog/CatVsDog.py
@@ -53,8 +53,30 @@ class CatandDogSet(Dataset):
         img_idx = self.img_name[idx]
         img_item_path = os.path.join(self.path, img_idx)
         img = Image.open(img_item_path)
-        label = img_idx.split(".")[0]
+        label = img_idx.split(".")[0] # 파일명에서 해당 사진의 label 추출
         return img, label, img_item_path
+
+class CustomSet(Dataset):
+    def __init__(self, imgs, class_to_int, path, mode, transforms):
+        self.imgs = imgs
+        self.class_to_int = class_to_int
+        self.mode = mode
+        self.transforms = transforms
+        self.path = path
+        
+    def __len__(self):
+        return len(self.imgs)
+    
+    def __getitem__(self, idx):
+        img = Image.open(self.path + self.imgs[idx])
+        if self.mode  in ["train" , "test"]:
+            label = self.class_to_int[self.imgs[idx].split(".")[0]]
+            img = self.transforms(img)
+            return img, label
+        elif self.mode == "pred":
+            pred_id = self.imgs[idx].split(".")[0]
+            img = self.transforms(img)
+            return img, pred_id      
 
 #--------------------------------------------------------
 # extract datasets
@@ -62,7 +84,35 @@ class CatandDogSet(Dataset):
 
 imgs = os.listdir(TRAIN_PATH)
 train_imgs = np.random.choice(imgs , 20000, replace=False) # 비복원 추출
-
 test_imgs = np.setdiff1d(imgs , train_imgs) # imgs과 train_imgs의 차집합
-
 pred_imgs = [f"{path}.jpg" for path in range(1,len(os.listdir(TEST_PATH))+1)]
+
+#--------------------------------------------------------
+# data transformation
+#--------------------------------------------------------
+
+class_to_int = {"cat" : 0 , "dog" : 1}
+train_transforms = transforms.Compose([
+    transforms.Resize((360 , 360)) , 
+    transforms.RandomHorizontalFlip(p = 0.5) , 
+    transforms.ToTensor() , 
+    transforms.Normalize((0 , 0 , 0) , (1 , 1 , 1))
+])
+
+test_pred_transforms = transforms.Compose([
+    transforms.Resize((360 , 360)) , 
+    transforms.ToTensor() , 
+    transforms.Normalize((0 , 0 , 0) , (1 , 1 , 1))
+])
+
+dataset = CatandDogSet(TRAIN_PATH)
+
+BATCH_SIZE = 20
+
+train = DataLoader(CustomSet(train_imgs , class_to_int , TRAIN_PATH , "train" , train_transforms) , batch_size = BATCH_SIZE , shuffle = True)
+test = DataLoader(CustomSet(test_imgs , class_to_int , TRAIN_PATH , "test" , test_pred_transforms) , batch_size = BATCH_SIZE , shuffle = True)
+pred = DataLoader(CustomSet(pred_imgs , class_to_int , TEST_PATH , "pred" , test_pred_transforms))
+
+img, label , path = dataset[np.random.randint(len(dataset))]
+plt.imshow(img)
+plt.show()
