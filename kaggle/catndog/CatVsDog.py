bf1d45b2e6681a4633a762a3ee3412e2f2bef278
haeunko <hekoh99@naver.com>
2022-06-28 14:02:38 +0900

    custom data

--- kaggle/catndog/CatVsDog.py
+++ kaggle/catndog/CatVsDog.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#%matplotlib inline 
 
 import os
 from sys import path
@@ -18,7 +17,43 @@ from torch.autograd import Variable
 from torchvision import transforms
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_PATH = "./train/"
-TEST_PATH = "./test1/"
+TRAIN_PATH = "./data/train/"
+TEST_PATH = "./data/test1/"
 
 print(DEVICE)
+
+#--------------------------------------------------------
+# un-zip the data
+#--------------------------------------------------------
+
+from pathlib import Path
+
+path = Path(TRAIN_PATH)
+if not (path).exists():
+    print("-- un-zip datasets --")
+    with ZipFile('./data/train.zip', 'r') as zip_ref:
+        zip_ref.extractall('./data')
+
+    with ZipFile('./data/test1.zip', 'r') as zip_ref:
+        zip_ref.extractall('./data')
+
+#--------------------------------------------------------
+# custom dataset
+#--------------------------------------------------------
+
+class CatandDogSet(Dataset):
+    def __init__(self, path):
+        self.path = path
+        self.img_name = os.listdir(self.path)
+                
+    def __len__(self):
+        return len(self.img_name)
+    
+    def __getitem__(self, idx):
+        img_idx = self.img_name[idx]
+        img_item_path = os.path.join(self.path, img_idx)
+        img = Image.open(img_item_path)
+        label = img_idx.split(".")[0]
+        return img, label, img_item_path
+
+imgs = os.listdir(TRAIN_PATH)
