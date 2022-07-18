2e2dc3f83b16d82ec7503cffb9aa2adf2430a67b
haeunko <hekoh99@naver.com>
2022-07-18 13:09:33 +0900

    setup

--- kaggle/epitope/predict.py
+++ kaggle/epitope/predict.py
@@ -15,6 +15,7 @@ import warnings
 warnings.filterwarnings(action='ignore')
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+print(device)
 
 # --------------------------------------------------------
 #   Hyper parameter setting
@@ -31,3 +32,37 @@ CFG = {
     'THRESHOLD':0.5,
     'SEED':41
 }
+
+# --------------------------------------------------------
+#   Set Random seed
+# --------------------------------------------------------
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+seed_everything(CFG['SEED'])
+
+# --------------------------------------------------------
+#   Data pre-processing
+# --------------------------------------------------------
+
+def get_preprocessing(data_type, new_df):
+    alpha_map = {
+                'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5,
+                'G':6, 'H':7, 'I':8, 'J':9, 'K':10, 'L':11,
+                'M':12, 'N':13, 'O':14, 'P':15, 'Q':16, 'R':17,
+                'S':18, 'T':19, 'U':20, 'V':21, 'W':22, 'X':23,
+                'Y':24, 'Z':25, '<PAD>':26,
+            }
+    epitope_list = []
+    left_antigen_list = []
+    right_antigen_list = []
+    label_list = []
+    
+    return epitope_list, left_antigen_list, right_antigen_list, label_list
