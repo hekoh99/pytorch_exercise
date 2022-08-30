56d614a6e94317967af67357e26a6c08bd25cd89
haeunko <hekoh99@naver.com>
2022-08-30 12:48:00 +0900

    original img

--- lab/plenoptic/extract_masked_img.py
+++ lab/plenoptic/extract_masked_img.py
@@ -1,3 +1,9 @@
 import cv2
 import numpy as np
+import pickle
 
+img = cv2.imread("./data/video_capture/taekgyun_jpg/000.jpg")
+cv2.imshow("original img", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
